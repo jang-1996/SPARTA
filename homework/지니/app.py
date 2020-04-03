8df6d989cea943d1bec83f03a3a4bb366777f3dc
jang-1996 <61787252+jang-1996@users.noreply.github.com>
2020-04-03 15:58:30 +0900

    지니

--- homework/지니/app.py
+++ homework/지니/app.py
@@ -7,8 +7,19 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 
 soup = BeautifulSoup(data.text, 'html.parser')
 musics = soup.select('div.newest-list > div.music-list-wrap > table > tbody > tr')
-print(musics)
+
+#print(musics)
 
 #############################
 # (입맛에 맞게 코딩)
-#############################
+###########f##################
+rank = 1
+for music in musics:
+    # movie 안에 a 가 있으면,
+    a_tag= music.select_one('td.info >  a')
+
+    if a_tag is not None:
+      title = a_tag.text
+      star = music.find('td', {'class':'info'}).find('a', {'class':'artist ellipsis'}).text
+      print(rank, title, star)
+      rank += 1
